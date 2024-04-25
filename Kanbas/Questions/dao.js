e186eb7c68cc7f90b42047dcfcf5090803008079
judymute <julie.lam1181@gmail.com>
2024-04-25 01:28:16 -0400

    create questions arrays

--- Kanbas/Questions/dao.js
+++ Kanbas/Questions/dao.js
@@ -1,7 +1,6 @@
 import model from "./model.js";
 
 export const createQuestion = (question) => {
-  delete question._id;
   return model.create(question);
 };
 
--- Kanbas/Questions/routes.js
+++ Kanbas/Questions/routes.js
@@ -4,7 +4,7 @@ export default function QuestionRoutes(app) {
   const createQuestion = async (req, res) => {
     try {
       const question = await dao.createQuestion(req.body);
-      console.log('create a question:' + question);
+      console.log('Created question:', question);
       res.json(question);
     } catch (err) {
       console.error('Error creating question:', err);
@@ -55,30 +55,4 @@ export default function QuestionRoutes(app) {
   app.post("/api/questions/current", current);
   app.get("/api/questions", findAllQuestions);
 
-//   app.put("/api/questions/:questionId", async (req, res) => {
-//     try {
-//       const { questionId } = req.params;
-//       const updatedQuestion = req.body;
-//   
-//       console.log("Updating question with ID:", questionId);
-//       console.log("Updated question data:", updatedQuestion);
-//   
-//       const question = await Question.findByIdAndUpdate(
-//         questionId,
-//         updatedQuestion,
-//         { new: true }
-//       );
-//   
-//       if (!question) {
-//         console.log("Question not found");
-//         return res.status(404).json({ error: "Question not found" });
-//       }
-//   
-//       console.log("Updated question:", question);
-//       res.json(question);
-//     } catch (error) {
-//       console.error("Error updating question:", error);
-//       res.status(500).json({ error: "Internal server error" });
-//     }
-//   });
 }
